c3c2ebf1fb42ca25e5eb93e7c9a2e31efbc8e648
MohammadReza Ranjbar <m.ranjbar80@gmail.com>
2022-01-05 10:43:48 +0330

    final

--- main.js
+++ main.js
@@ -1,33 +1,22 @@
 require('dotenv').config();
 const db = require('./DB');
-const User = require('./Model/User')
 const DAO = require('./DAO');
-
-// async function main () {
-//     try {
-//         await sql.connect(sqlConfig)
-//         const result = (await sql.query(`select top 1 * from Bus_Reservations`)).recordsets[0]
-//         // console.log(result[0])
-//         const keys = Object.keys(result[0])
-//         // const user = new User(result[0][keys[0]], result[0][keys[1]], result[0][keys[2]], result[0][keys[3]], result[0][keys[4]], result[0][keys[5]], result[0][keys[6]], result[0][keys[7]], result[0][keys[8]], result[0][keys[9]], result[0][keys[10]], result[0][keys[11]], result[0][keys[12]], result[0][keys[13]] )
-//         // console.log(user)
-//         // console.log(keys)
-//         let st = ''
-//         keys.map((el) => {
-//             st += el+','
-//         })
-//         console.log(st)
-//     } catch (err) {
-//         console.log(err)
-//     }
-// }
-// main()
-
-
-
 
 (async function () {
     global.sql = await db.connect()
-    const result = await DAO.BusTripDAO.findAll()
-    console.log(result)
+    // console.log(await DAO.BusReservationDAO.findAll())
+    // console.log(await DAO.BusStopDAO.findAll())
+    // console.log(await DAO.BusTripDAO.findAll())
+    // console.log(await DAO.FlightDAO.findAll())
+    // console.log(await DAO.FlightsDomesticDAO.findAll())
+    // console.log(await DAO.FlightsForeignDAO.findAll())
+    // console.log(await DAO.FlightsForeignStopDAO.findAll())
+    // console.log(await DAO.FlightsReservationDAO.findAll())
+    // console.log(await DAO.HotelDAO.findAll())
+    // console.log(await DAO.HotelsEquipmentDAO.findAll())
+    // console.log(await DAO.RoomDAO.findAll())
+    // console.log(await DAO.RoomOptionDAO.findAll())
+    // console.log(await DAO.RoomReservationDAO.findAll())
+    // console.log(await DAO.TransactionDAO.findAll())
+    console.log(await DAO.UserDAO.findAll())
 }())
